a96ea6f67fa630e4dd36fc1bc5955e0548ff4a28
tigerlab <tiggers12@hotmail.com>
2023-08-10 15:34:45 +0700

    okk

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -17,7 +17,7 @@ export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
         <span>{replyText}</span>
         <div className="d-flex align-items-center gap-1">
           <img src="/like.svg" width={20}></img>
-          <span className="text-muted">{likeNum}</span>
+          <span className="text-muted">{likeNum}คน</span>
         </div>
       </div>
     </div>

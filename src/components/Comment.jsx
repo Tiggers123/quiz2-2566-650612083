3ca65728128d9e8a1b2d8c7527e1298a7b01f1a7
tigerlab <tiggers12@hotmail.com>
2023-08-10 15:34:17 +0700

    ppp

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -28,7 +28,7 @@ export const Comment = ({
           {likeNum > 0 ? (
             <div className="d-flex align-items-center gap-1">
               <img src="/like.svg" width={20}></img>
-              <span className="text-muted">{likeNum}</span>
+              <span className="text-muted">{likeNum} คน</span>
             </div>
           ) : (
             ""
